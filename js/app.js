277fffeb3c6c8a95008f52157a445d37d5b4eb1d
Lessar277 <lessar277@gmail.com>
2015-09-08 20:18:27 +0300

    basic version done

--- js/app.js
+++ js/app.js
@@ -1,30 +1,65 @@
 //main div height and width, used to calculate small divs;
 var $containerWidth = $('#container').width();
 var $containerHeight = $('#container').height();
-//define small div to be appended to container
-var $smallDiv = $('<div class="smallDiv"></div>');
-//ask the user what size his sketchpad will be;
-function ask(){
-    divCounter = prompt('Select a grid width:');
+
+//function to generate random rgb color
+function randomRGB() {
+    var randNum = function () {return Math.floor(Math.random() * 256);};
+    return 'rgb(' + randNum() + ', ' + randNum() + ', ' + randNum() + ')';
 }
+//function to generate increasingly black opacity;
 
 
-function addDiv() {
-    $('#container').append($smallDiv.clone());
-}
-
-ask();
-
-//and set the small divs height and width based on user input;
-var $smallDivWidth = $containerWidth / divCounter + 'px';
-var $smallDivHeight = $containerHeight / divCounter + 'px';
-function setDimensions(){
+//ask the user what size his sketchpad will be and set dimension vars
+//appropriately;
+//this function will set the grid values as per the user's input
+function gridSettings(){
+    //clear contents of main container div;
+    $('#container').empty();
+    //ask user for width of grid and check to see it's a valid number smaller than 100;
+    var divCounter = prompt('Select a grid width (or leave blank for default(16x16):');
+        if(isNaN(divCounter)){
+            divCounter = prompt('Oops. That is not a number. Please enter the desired grid width again:');
+        } else if(divCounter>100) {
+            divCounter = prompt('That number is too large and it will murder your browser in your sleep. Choose again:');
+        } else if(divCounter === '') {
+            divCounter = 16;
+        }
+    //declare DOM object to be inserted and variables to make width relative to parent container;
+    var $smallDiv = $('<div class="smallDiv"></div>');
+    var $smallDivWidth = $containerWidth / divCounter + 'px';
+    var $smallDivHeight = $containerHeight / divCounter + 'px';
+    //add squares to grid based on user input and set its dimensions according to the above formula;
+    for (i = 1; i <= divCounter * divCounter; i++){
+        $('#container').append($smallDiv.clone());
+    }
     $('.smallDiv').css("width", $smallDivWidth);
     $('.smallDiv').css("height", $smallDivHeight);
 }
-// loop to append small divs to html document;
-for (i = 1; i <= divCounter * divCounter; i++){
-    addDiv();
-}
 
-setDimensions();
+//This prompts and loads the grid with a default black mouseover;
+function setGridDefault() {
+    $(gridSettings()).ready(function(){
+        $('.smallDiv').mouseenter(function(){
+            $(this).css('background-color', 'white');
+        });
+        $('.smallDiv').click(function(){
+            $(this).css('background-color', 'black');
+        });
+    });
+}
+//Mouseover will have different random colors
+function setGridRandom() {
+    $(gridSettings()).ready(function(){
+        $('.smallDiv').mouseenter(function(){
+            $(this).css('background-color', randomRGB());
+        });
+        $('.smallDiv').click(function(){
+            $(this).css('background-color', 'black');
+        });
+    });
+}
+//function to clean the grid without resetting it
+function cleanGrid () {
+    $('.smallDiv').css('background-color', 'black')
+}
